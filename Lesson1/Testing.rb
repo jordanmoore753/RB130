5f08a10473f7de9bbe5cefb823fcb314efffdfb0
Jordan Moore <46928924+jordanmoore753@users.noreply.github.com>
2019-05-26 09:25:46 -0400

    Update Notes

--- Lesson1/Testing.rb
+++ Lesson1/Testing.rb
@@ -1,3 +1,13 @@
-my_file = File.open("test_file.txt", "w+")
-my_file.write('I am a governor.')
-my_file.close
+def test2(block)
+  puts 'hello from test2'
+  block.call
+  puts 'goodbye from test2'
+end
+
+def test(&block)
+  puts 'first in test'
+  test2(block)
+  puts 'second in test'
+end
+
+test { puts 'test block' }
